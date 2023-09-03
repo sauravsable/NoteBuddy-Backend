9e2cfa1d3e35ffdb20b7098de42034a52335e013
saurav <sauravsable4102@gmail.com>
2023-09-03 18:31:51 +0530

    secret changed

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cors({origin:'https://sauravnotebuddy.netlify.app',credentials:true}));
 
 app.use(session({
-secret:"NoteBuddy",
+secret:"Notebuddy",
 resave:false,
 saveUninitialized:false,
 }));
