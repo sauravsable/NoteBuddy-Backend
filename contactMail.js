9d20e3b0d218587623a6766ff4ba0a7f292a9e12
Sharad Kumar <114913730+sharad-12@users.noreply.github.com>
2024-09-06 10:15:41 +0530

    Update contactMail.js

--- contactMail.js
+++ contactMail.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const {google} = require('googleapis');
 const config = require('./config');
 const OAuth2 = google.auth.OAuth2;
-
+// setting oauth client
 const OAuth2_client = new OAuth2(config.clientID,config.clientSecret);
 OAuth2_client.setCredentials({refresh_token:config.refreshToken});
 
